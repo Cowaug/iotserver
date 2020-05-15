60b14099235806da4f6f9227cfb98dc0794aa27f
Nguyen Thanh Binh <ntb9a1@gmail.com>
2020-05-15 22:52:44 +0700

    Added comment

--- src/main/java/com/iot/iotserver/DeviceType.java
+++ src/main/java/com/iot/iotserver/DeviceType.java
@@ -1,12 +1,20 @@
 package com.iot.iotserver;
 
 public enum DeviceType {
-    LIGHT_SENSOR("lightSensor"),
-
-    ;
+    SENSOR_LIGHT("lightSensor"),
+    SENSOR_TEMP("temperatureSensor"),
+    SENSOR_PLANT("plantSensor"),
+    IDENT_LIGHT("indicateLight"),
+    LIGHT_BULB("lightBulb"),
+    AIR_CONDITIONER("airConditioner"),
+    MOTOR("motor");
 
     private final String value;
     DeviceType(String value){
         this.value=value;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
