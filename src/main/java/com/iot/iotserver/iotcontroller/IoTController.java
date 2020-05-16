138f9b13803fc7e3f98d5e287024be74e79cc71b
Nguyen Thanh Binh <ntb9a1@gmail.com>
2020-05-16 20:59:54 +0700

    Added threshold value in IoT Controller

--- src/main/java/com/iot/iotserver/iotcontroller/IoTController.java
+++ src/main/java/com/iot/iotserver/iotcontroller/IoTController.java
@@ -5,8 +5,44 @@ package com.iot.iotserver.iotcontroller;
  * After process data, it will send the data back to the MQTT server by calling methods from MQTTConnection class
  */
 public class IoTController {
+    private static float temperatureThreshold = 27; // default is 27 Celsius
+    private static float humidityThreshold = 70;// default is 70 percent
+    private static int lightThreshold = 650;// default 650 lumen
+    private static int plantThreshold = 50;// default 50%
 
     public static void processData(String deviceType, String... sensorValues){
         //todo control IoT application based on given value
+    }
+
+    /**
+     * Set light sensor threshold
+     * @param lightThreshold Value between 0 and 1023
+     */
+    public static void setLightThreshold(int lightThreshold) {
+        IoTController.lightThreshold = lightThreshold;
+    }
+
+    /**
+     * Set plant humidity sensor threshold
+     * @param plantThreshold Value between 0 and 100
+     */
+    public static void setPlantThreshold(int plantThreshold) {
+        IoTController.plantThreshold = plantThreshold;
+    }
+
+    /**
+     * Set temperature sensor threshold
+     * @param temperatureThreshold Value between 0 and 100
+     */
+    public static void setTemperatureThreshold(float temperatureThreshold) {
+        IoTController.temperatureThreshold = temperatureThreshold;
+    }
+
+    /**
+     * Set humidity sensor threshold
+     * @param humidityThreshold Value between 0 and 100
+     */
+    public static void setHumidityThreshold(float humidityThreshold) {
+        IoTController.humidityThreshold = humidityThreshold;
     }
 }
